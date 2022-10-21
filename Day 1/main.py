b514d212f428096c9b817f3824780affae4368a9
aeoncloud <104237849+aeoncloud@users.noreply.github.com>
2022-10-22 00:07:59 +0100

    Update main.py

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,8 +1,7 @@
-print ("""Devesh
-21st October 2022
-I am signing up for Replit's 100 days of of Python challenge!
+print("Devesh")
+print("October 21, 2022")
+print("""I am signing up for Replit's 100 days of Python challenge!
 I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
-I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!
-I'm feeling \U0001f600
-You can foilow my progress at replit.com/@deveshincloud
-This is really fun""")
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!""")
+print("I am feeling \U0001f600")
+print("You can follow my progress at replit.com/@deveshincloud")
