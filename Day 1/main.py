fb95b26e9146043758f0c09d40c3e7e73066803a
aeoncloud <104237849+aeoncloud@users.noreply.github.com>
2022-10-21 23:36:39 +0100

    Create main.py

---
+++ Day 1/main.py
@@ -0,0 +1,8 @@
+print ("""Devesh
+21st October 2022
+I am signing up for Replit's 100 days of of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!
+I'm feeling \U0001f600
+You can foilow my progress at replit.com/@deveshincloud
+This is really fun""")
