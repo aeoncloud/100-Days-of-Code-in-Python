b58ff085911b021729cec29b3d107455e5f3302a
aeoncloud <104237849+aeoncloud@users.noreply.github.com>
2022-10-26 23:13:11 +0100

    Create main.py

---
+++ Day 5/main.py
@@ -0,0 +1,23 @@
+print("Which character are you from 'Avengers?'")
+print()
+print("Answer these questions and let's find out.")
+print()
+print("Please use Y or N for Yes and Nno.")
+
+likeGreen = input("Do you like the color green?: ")
+if likeGreen == "Y":
+  print("Aha! You must be the Hulk!")
+
+IronIsCool = input("Do you think building robots is cool?: ")  
+if IronIsCool == "Y":
+  print("Aha! You must be Iron Man. Cool suit!")
+
+TimeTravel = input("Do you like traveling through time?: ")  
+if TimeTravel == "Y":
+  print("Aha! You must be Captain America! Hi!")
+
+Strong = input("Are you super strong?: ")
+if Strong == "Y":
+  print("WOW! You have got to be Thor!")
+else:
+  print("How sad...I guess you are not like anyone from 'Avengers.'")
