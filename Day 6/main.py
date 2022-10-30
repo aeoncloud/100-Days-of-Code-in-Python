39ed23241ac37f0bebdd536bef5a18849f6c0281
aeoncloud <104237849+aeoncloud@users.noreply.github.com>
2022-10-30 23:21:24 +0000

    Update main.py

--- Day 6/main.py
+++ Day 6/main.py
@@ -1,1 +1,13 @@
-print("This is a test")
+print("MY LOGIN SYSTEM")
+print("+++++++++++++++")
+username = input("Please enter your Username: ")
+password = input("Please enter your Password: ")
+
+if username == "Devesh" and password == "password":
+  print("Hi Devesh! It is great to have you back! How was your holiday? I hope you had a wonderful time in the Alps!")
+elif username == "David" and password == "password":
+  print("Hello David. You look lovely today! You could have logged in without a passwowrd to be honest!")
+elif username == "Moss" and password == "password":
+  print("Hello there Moss! I saw you on telly last night, with Roy!")
+else:
+  print("STOP RIGHT THERE! IDENTIFY YOURSELF!")
