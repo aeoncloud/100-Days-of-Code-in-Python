d4d9bc9f92523d4aad01f89c5bf193076bb5c799
aeoncloud <104237849+aeoncloud@users.noreply.github.com>
2022-10-30 23:24:53 +0000

    Update main.py

--- Day 6/main.py
+++ Day 6/main.py
@@ -3,11 +3,11 @@ print("+++++++++++++++")
 username = input("Please enter your Username: ")
 password = input("Please enter your Password: ")
 
-if username == "Devesh" and password == "password":
-  print("Hi Devesh! It is great to have you back! How was your holiday? I hope you had a wonderful time in the Alps!")
-elif username == "David" and password == "password":
-  print("Hello David. You look lovely today! You could have logged in without a passwowrd to be honest!")
-elif username == "Moss" and password == "password":
-  print("Hello there Moss! I saw you on telly last night, with Roy!")
+if username == "Devesh" and password == "PyThonRocks":
+  print("Welcome, Devesh! You are looking good today!")
+elif username == "Moss" and password == "Repl!t4evEr":
+  print("Hi Moss! Love your hair today!")
+elif username == "Roy" and password == "SmashTHEbugs!":
+  print("Yo Roy! What up?!")
 else:
   print("STOP RIGHT THERE! IDENTIFY YOURSELF!")
