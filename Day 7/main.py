d7de9d83d0133874617ed8ef26e672c0bbe9ed72
aeoncloud <104237849+aeoncloud@users.noreply.github.com>
2022-11-02 23:41:46 +0000

    Update main.py

--- Day 7/main.py
+++ Day 7/main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+print("***FAKE ANIME FAN FINDER***")
+print("+++++++++++++++++++++++++++")
+print("Answer these questions to find out.")
+
+name = input("What is the name of Sasuke's Brother? " )
+if name == "Itachi":
+  print("Wow, you just got lucky!")
+  rival = input("Who is Sasuke's Rival? ")
+  if rival == "Naruto":
+    print("WOW YOU ARE A TRUE NARUTO FAN!!!")
+  elif rival == "Gara??":
+    print("What? No, you're wrong!!.")
+  else:
+    print("I KNEW IT!! FAKE NARUTO FAN!!!")
